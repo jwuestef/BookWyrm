e50637cdd73f7a8294a2571b2cf4bcfd8ee37d81
Jeremy Wuestefeld <jwuestef@gmail.com>
2018-11-30 12:16:03 -0500

    Self-Service Check-Out Kiosk is working

--- BookWyrm.Web/Controllers/HomeController.cs
+++ BookWyrm.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -101,14 +102,32 @@ namespace BookWyrm.Web.Controllers
                     return Json(new { message = "Invalid user barcode, please try again" });
                 }
 
-                string fullName = foundUser.FirstName + " " + foundUser.LastName;
+                // Get the list of this user's currently checked out books
+                using (BookDb _bookDb = new BookDb())
+                {
+                    var currentlyCheckedOutBooks = _bookDb.Borrowings
+                        .Where(bw => bw.UserId == foundUser.Id && bw.CheckInDateTime == null)
+                        .Join(_bookDb.Books,
+                            bw => bw.BookId,
+                            bk => bk.BookId,
+                            (borrowing, book) =>
+                            new
+                            {
+                                Title = book.Title,
+                                Author = book.Author,
+                                DueDate = borrowing.DueDate.ToString(),
+                                IsLate = borrowing.DueDate < DateTime.Now
+                            }
+                        ).ToList();
 
-                // We found the user, reply and tell the user it's okay to start scanning books
-                // TODO: return list of their currently checked out books and display on screen
-                return Json(new {
-                    fullName = foundUser.FirstName + " " + foundUser.LastName,
-                    userId = foundUser.Id
-                });
+
+                    return Json(new
+                    {
+                        fullName = foundUser.FirstName + " " + foundUser.LastName,
+                        userId = foundUser.Id,
+                        currentlyCheckedOutBooks = currentlyCheckedOutBooks
+                    });
+                }
             }
         }
 
@@ -134,6 +153,7 @@ namespace BookWyrm.Web.Controllers
                     return Json(new { message = "Invalid book barcode, please try again" });
                 }
 
+                // We also have been given a userId, search the database for this user
                 using (IdentityDb _identityDb = new IdentityDb())
                 {
                     ApplicationUser foundUser = _identityDb.Users.Find(userId);
@@ -141,6 +161,29 @@ namespace BookWyrm.Web.Controllers
                     {
                         Response.StatusCode = (int)HttpStatusCode.BadRequest;
                         return Json(new { message = "Invalid user barcode, please try again" });
+                    }
+
+                    // Check to make sure this user doesn't already have this book checked out - no duplicate checkouts allowed
+                    var isAlreadyCurrentlyCheckedOutBySameUser = _bookDb.Borrowings.Any(bw => 
+                        bw.BookId == foundBook.BookId
+                        && bw.UserId == foundUser.Id
+                        && bw.CheckInDateTime == null
+                    );
+                    if (isAlreadyCurrentlyCheckedOutBySameUser)
+                    {
+                        Response.StatusCode = (int)HttpStatusCode.Conflict;
+                        return Json(new { message = "You already have this book checked out" });
+                    }
+
+                    // Check to make sure this book isn't checked out by someone else - no duplicate checkouts allowed
+                    var isAlreadyCurrentlyCheckedOutBySomeoneElse = _bookDb.Borrowings.Any(bw =>
+                        bw.BookId == foundBook.BookId
+                        && bw.CheckInDateTime == null
+                    );
+                    if (isAlreadyCurrentlyCheckedOutBySomeoneElse)
+                    {
+                        Response.StatusCode = (int)HttpStatusCode.Conflict;
+                        return Json(new { message = "Someone else has this book checked out - please contact a librarian" });
                     }
 
                     // Make sure the user is old enough to meet the book's minimum age requirement
@@ -155,7 +198,7 @@ namespace BookWyrm.Web.Controllers
                         return Json(new { message = "You do not meet the minimum age requirement to check out this book" });
                     }
 
-                    // Now create an entry in the borrowing table
+                    // Create an entry in the borrowing table
                     Borrowing newBorrowing = new Borrowing()
                     {
                         BorrowingId = Guid.NewGuid(),
@@ -171,13 +214,11 @@ namespace BookWyrm.Web.Controllers
                     // Check out for this book complete, return the book's information
                     return Json(new
                     {
-                        newBorrowing = newBorrowing,
+                        dueDate = newBorrowing.DueDate.ToString("MMM dd yyyy hh:mmtt"),
                         bookDetails = new
                         {
                             Title = foundBook.Title,
-                            Author = foundBook.Author,
-                            ISBN = foundBook.ISBN,
-                            Barcode = foundBook.Barcode
+                            Author = foundBook.Author
                         }
                     });
 
